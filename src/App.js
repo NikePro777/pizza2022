5063862b4289fe48090ba3ed7b79df8275d5809a
Evgeniy Datskij <evgenij.datskij@mail.ru>
2024-06-27 16:13:45 +0500

    fix mistakes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import Pizza from './components/Pizza';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
-            <Pizza price={500} title="Вегетерианская" />
-            <Pizza />
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock price={500} title="Вегетерианская" />
+            <PizzaBlock />
           </div>
         </div>
       </div>
