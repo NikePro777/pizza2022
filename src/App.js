a83e995dffb35e4fb44989ebf2f7fe49fce72cdc
Evgeniy Datskij <evgenij.datskij@mail.ru>
2024-08-06 10:42:46 +0500

    adaptive cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -11,7 +11,6 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          {/* <Home /> */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
